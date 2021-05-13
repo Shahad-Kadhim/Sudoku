3aac2fe79d00c0a81a4a42cf14a61fb12e835d90
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-05-14 01:41:30 +0300

    design ui

---
+++ app/src/main/java/com/example/sudoku/Game.kt
@@ -0,0 +1,18 @@
+package com.example.sudoku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+    fun goToHome(v: View){
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
+    }
+
+}
